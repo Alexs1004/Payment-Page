495a84e9f69c261d3d1a52acfb5abe19a3117432
Alexandre Stutz <astutz@pubfac.com>
2024-02-26 17:59:00 +0100

    add modal content

--- frontend/script.js
+++ frontend/script.js
@@ -341,9 +341,9 @@ window.addEventListener('scroll', function() {
 //     <div class="row">
 //         <!-- Bloc SEPA -->
 //         <div id="sepaBlock" class="col-lg-6">
-//             <!-- Contenu du bloc SEPA -->
-//             <!-- Copiez et collez le contenu du bloc SEPA ici -->
-//         </div>
+// //             <!-- Contenu du bloc SEPA -->
+// //             <!-- Copiez et collez le contenu du bloc SEPA ici -->
+// //         </div>
 //         <!-- Bloc Carte de crédit -->
 //         <div id="creditCardBlock" class="col-lg-6" style="display:none;">
 //             <!-- Contenu du bloc Carte de crédit -->
